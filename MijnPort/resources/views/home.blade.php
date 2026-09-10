391a5e70af9f33f15a0173865af2cdfaaca4b438
BoredxManiac <Manicalsome.1@gmail.com>
2026-09-10 13:35:25 +0200

    werkende project toegevoegd.

---
+++ MijnPort/resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+ <h1>Hello</h1>
+ <div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit et nobis dicta adipisci rem, dolor amet placeat numquam non culpa nemo est ullam quaerat repudiandae voluptatibus! Nobis rem optio officiis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores distinctio, laudantium deserunt minus harum dolorem iste modi sequi quos in totam, quas architecto et hic fugiat cum blanditiis, suscipit esse.</p>
+ </div>
+ <div>
+ <img src="{{ asset('images/placeholder.png') }}" alt="placeholder image">
+ </div>
+
+</body>
+</html>
