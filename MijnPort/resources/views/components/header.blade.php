a2316b015a4ed983ff7bff91395da367a72d651e
BoredxManiac <Manicalsome.1@gmail.com>
2026-09-10 15:26:20 +0200

    header aangemaakt + gestyled

---
+++ MijnPort/resources/views/components/header.blade.php
@@ -0,0 +1,10 @@
+<header class="bg-blue-600">
+<div class="flex justify-between">
+<h1>mijn portofolio</h1>
+<nav>
+<a href="">websites</a>
+<a href="">contact</a>
+</nav>
+</div>
+</header>
+{{ $slot }}
