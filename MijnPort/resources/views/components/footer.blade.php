9af4b081c35ad229db72388d4a7956059d3c4fbf
BoredxManiac <Manicalsome.1@gmail.com>
2026-09-10 15:26:07 +0200

    footer aangemaakt + styling aangepast

--- MijnPort/resources/views/components/footer.blade.php
+++ MijnPort/resources/views/components/footer.blade.php
@@ -1,1 +1,11 @@
+{{ $slot }}
+<footer class="bg-blue-600 mt-auto">
+<div class="flex justify-between">
+<h1>mijn portofolio</h1>
+<nav>
+<a href="{{ route('/websites') }}">websites</a>
+<a href="">contact</a>
+</nav>
+</div>
+</footer>
 
