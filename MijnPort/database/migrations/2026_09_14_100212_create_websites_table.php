df1e3ada3a87f2cbfa42b58f0a9f25c66af4e9fc
BoredxManiac <Manicalsome.1@gmail.com>
2026-09-14 12:28:18 +0200

    migration for the ERD aangemaakt + route aangemaakt voor projects crudsysteem

---
+++ MijnPort/database/migrations/2026_09_14_100212_create_websites_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('websites', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('filter_options_id')->nullable()->constrained('filter_options')->nullOnDelete();
+            $table->string('title');
+            $table->text('description');
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('websites');
+    }
+};
