4b046c34ef8ca4036f9205205f771465d3df50b6
BoredxManiac <Manicalsome.1@gmail.com>
2026-09-19 21:32:44 +0200

    grid systeem gefixed

--- resources/views/websites.blade.php
+++ resources/views/websites.blade.php
@@ -1,39 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-layouts.base>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
+    <body class="flex min-h-1/2 flex-col">
 
-<body class="flex min-h-1/2 flex-col">
-    <x-header />
-    <main>
+        <main>
 
-        <div>
-            <h1 class="text-center mt-5 mb-5 ">WELCOMEEEEE</h1>
-            <a href="{{ route('projects.create') }}">create</a>
-        </div>
-
-         @foreach ($websites as $website)
-
-          <div class="grid  grid-rows-[repeat (3, 5px)] grid-cols-[repeat(3,auto)] w-full ">
-                 <div class="flex flex-row">
-                <img src="{{ asset('storage/images/' . $website->image )}}" alt="">
-                <h3>{{ $website->title }}</h3>
+            <div>
+                <h1 class="text-center mt-5 mb-5 ">WELCOMEEEEE</h1>
+                <a href="{{ route('projects.create') }}">create</a>
             </div>
-          </div>
-
-          @endforeach
-
-        {{-- <
-        <div class="flex flex-row">
+             <div class="flex flex-row">
             <div class="w-[30%]">
             <p>lalalall</p>
             </div>
+            <div class="grid grid-cols-3 w-full gap-4">
+                @foreach ($websites as $website)
+                <div>
+                    <img src="{{ asset('storage/images/' . $website->image) }}" alt="">
+                    <h3>{{ $website->title }}</h3>
+                    </div>
+                @endforeach
+            </div>
+
+            {{-- <
+
         <div class="grid  grid-rows-[repeat (3, 5px)] grid-cols-[repeat(3,auto)] w-full ">
             <div class="">
                 <img src="https://placehold.co/200x200" alt="">
@@ -53,8 +42,7 @@
             </div>
         </div>
           </div> --}}
-    </main>
-    <x-footer />
-</body>
+        </main>
 
-</html>
+    </body>
+</x-layouts.base>
