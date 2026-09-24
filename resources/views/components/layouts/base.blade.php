ca2f88a4dcc38c084e4e4b7f726d5d6660109f1b
BoredxManiac <Manicalsome.1@gmail.com>
2026-09-24 16:32:58 +0200

    authenticatie toegevoegd aan de login en logout knop

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -14,10 +14,15 @@
     <header class="bg-[#0E6BA8] rounded-b-xl p-5">
         <div class="flex justify-between p-4">
             <h1>mijn portofolio</h1>
-            <nav>
+            <nav class="flex gap-4">
                 <a href="{{ route('home') }}">home</a>
                 <a href="{{ route('websites') }}">websites</a>
-                <a href="{{ route('login') }}">login</a>
+                @guest<a href="{{ route('login') }}">login</a>@endguest
+                @auth<form action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <button type="submit">Logout</button>
+                </form>
+                @endauth
             </nav>
         </div>
     </header>
@@ -26,11 +31,11 @@
 
     <footer class="mt-2 bg-[#0E6BA8] mt-auto">
         <div class="flex justify-between p-4 mt-8 items-end">
-             <ul class="mt-">
+            <ul class="mt-">
                 <li class="mb-2">My portfolio</li>
-                 <li class="mb-2">A website made to showcase projects made</li>
-                 <li>website was designed and coded by Khadija Aweys M&copy;</li>
-             </ul>
+                <li class="mb-2">A website made to showcase projects made</li>
+                <li>website was designed and coded by Khadija Aweys M&copy;</li>
+            </ul>
         </div>
     </footer>
 
